1a0610ba2714dd4dafd59f9d9135632095a6eb8f
David Gonzalez-Velez <dave8@cs.uchicago.edu>
2020-03-11 23:29:56 -0500

    working on project

--- data_scraping.py
+++ data_scraping.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import sys
 import csv
+
+
+def clean_data(df):
+	df = df.transpose()
+    df = df.set_index(pd.Index(list(range(len(df)))))
+    for index, row in df.iterrows():
+    	if row['ages'] != '0':
+    		value = df.at[index, 'ages'][2:4] + '-' + df.at[index, 'ages'][-4:-2]
+		else:
+			value = 'No age specified'
+		df.set_value(index, 'ages', value)
+
+    
+
