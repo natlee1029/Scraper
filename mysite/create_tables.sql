958ce73342e429b271e4e1ea81dc9173279fe417
Nathan Lee <nathanlee@cs.uchicago.edu>
2020-03-12 12:51:38 -0500

    changes part 2

---
+++ mysite/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE program_info (
+   id integer,
+   ages varchar(17),
+   deadline integer,
+   fee integer,
+   subject varchar(50),
+   country varchar(20),
+   city varchar(30),
+   minimum_cost integer,
+   program_url varchar(350)
+);
+
+
+.import demo_cata.csv program_info
